c917bc2b8e959554001b450d4834227d67b6a92d
Maarten <maarten.postma1000@gmail.com>
2023-08-02 21:24:10 +0200

    Added some async fetch api functions
    I need to retrieve all the times of all the cards of a board.
    Im still in the middle of making the logic of this loop

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-fetch(
-  "https://api.trello.com/1/cards/64aceeef5a7fb8b7aa03f674/pluginData?key=07e64362b442fbd4613d132556e518c0&token=ATTA5339e95d2b6c82274949c448ca47d0ca51c40d3107340ceae8407ced5a8bb155BF7502A0"
-)
-  .then((res) => res.json())
-  .then((data) => console.log(data[0].value));
+// fetch(
+//   "https://api.trello.com/1/cards/64aceeef5a7fb8b7aa03f674/pluginData?key=07e64362b442fbd4613d132556e518c0&token=ATTA5339e95d2b6c82274949c448ca47d0ca51c40d3107340ceae8407ced5a8bb155BF7502A0"
+// )
+//   .then((res) => res.json())
+//   .then((res) => console.log(res[0].value));
+
+//get all the cards from specific trello board and put them in an array
+const key = "07e64362b442fbd4613d132556e518c0";
+const token =
+  "ATTA5339e95d2b6c82274949c448ca47d0ca51c40d3107340ceae8407ced5a8bb155BF7502A0";
+let getAllCardsUrl = `https://api.trello.com/1/boards/64c7f12d30d071439e4d0f07/cards?key=${key}&token=${token}`;
+let getActivityUrl = `https://api.trello.com/1/cards/${cardId}/pluginData?key=${key}&token=${token}`;
+
+getAllCardsFromBoard(getAllCardsUrl);
+getTimeActivity(getActivityUrl);
+
+async function getAllCardsFromBoard(url) {
+  const response = await fetch(url);
+  const allCards = await response.json();
+  allCards.forEach((card) => {
+    console.log(card.id);
+    getTimeActivity(card);
+  });
+}
+
+async function getTimeActivity(url) {
+  const response = await fetch(url);
+  const timeActivity = await response.json();
+  console.log(timeActivity);
+  timeActivity.forEach((el) => {
+    console.log(el.value);
+  });
+}
